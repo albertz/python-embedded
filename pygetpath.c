11b0128709a65a48bb0a8251308907dbd7db5b7a
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2013-08-03 00:33:10 +0200

    better

--- pygetpath.c
+++ pygetpath.c
@@ -8,26 +8,44 @@ extern char *Py_GetProgramName(void);
 
 static int pathCalculated = 0; 
 static char progPath[MAXPATHLEN+1];
+static char pylibPath[MAXPATHLEN+1];
 static char modulePathes[4*MAXPATHLEN+1];
 static char execPrefixPath[2*MAXPATHLEN+1];
 
-static void calcPathes() {
-	if(pathCalculated) return;
-
-	char* p = stpcpy(progPath, Py_GetProgramName());
-	while(--p > progPath) {
+static char* removeLastDir(char* dir, char* p) {
+	while(--p > dir) {
 		if(*p == '/') {
 			*p = 0;
 			break;
 		}
 	}
+	return p;
+}
+
+static void calcPathes() {
+	if(pathCalculated) return;
+
+	char* p = stpcpy(progPath, Py_GetProgramName());
+	p = removeLastDir(progPath, p);
 	
-	strcpy(modulePathes, progPath);
-	strcat(modulePathes, "/pylib/pylib.zip");
-	strcat(modulePathes, progPath);
-	strcat(modulePathes, "/pylib/otherlibs");
-	strcpy(execPrefixPath, progPath);
-	strcat(execPrefixPath, "/pylib/exec");
+#ifdef __APPLE__
+#include "TargetConditionals.h"
+#ifdef TARGET_OS_MAC
+	// the binary is in its own dir (Contents/MacOS),
+	// but we want (Contents/Resources).
+	p = removeLastDir(progPath, p);
+	strcat(progPath, "/Resources");
+#endif
+#endif
+	
+	strcpy(pylibPath, progPath);
+	strcat(pylibPath, "/pylib");	
+	strcpy(modulePathes, pylibPath);
+	strcat(modulePathes, "/pylib.zip");
+	strcat(modulePathes, pylibPath);
+	strcat(modulePathes, "/otherlibs");
+	strcpy(execPrefixPath, pylibPath);
+	strcat(execPrefixPath, "/exec");
 	
 	pathCalculated = 1;
 }
@@ -46,7 +64,7 @@ Py_GetPrefix(void)
 {
 	calcPathes();
 	// TODO: not sure if this is good / correct / makes sense
-	return progPath;
+	return pylibPath;
 }
 
 char *
