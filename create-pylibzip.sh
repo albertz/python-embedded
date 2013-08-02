a7d00b0239a2f01edc846aea8e443b00623d536f
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2013-08-03 00:14:26 +0200

    more

--- create-pylibzip.sh
+++ create-pylibzip.sh
@@ -4,6 +4,19 @@ cd $(dirname $0)
 
 pushd CPython/Lib || exit 1 
 rm pylib/pylib.zip
-zip -9 ../../pylib/pylib.zip -x test\* -x plat\* -x unittest -r *
+zip -9 ../../pylib/pylib.zip \
+	-x test\* \
+	-x plat\* \
+	-x unittest\* \
+	-x lib2to3\* \
+	-x lib-tk\* \
+	-x distutils\* \
+	-x ctypes/test\* \
+	-x email/test\* \
+	-x json/tests\* \
+	-x sqlite3/test\* \
+	-x bsddb\* \
+	-x idlelib\* \
+	-r *
 
 popd
