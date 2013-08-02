96151ffdaf67a5dc69e75553e09272419c831613
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2013-08-02 22:15:11 +0200

    more

--- create-xcode.py
+++ create-xcode.py
@@ -8,5 +8,16 @@ sys.path += ["mod-pbxproj"]
 from mod_pbxproj import *
 
 proj = XcodeProject.Load("Xcode-Python-empty.xcodeproj/project.pbxproj")
+
+src = proj.get_or_create_group('src')
+
+import compile
+def add_file(fn):
+	#print fn
+	proj.add_file(fn, parent=src)
+
+for fn in list(compile.baseFiles) + list(compile.modFiles) + list(compile.objFiels) + list(compile.parserFiles):
+	add_file(fn)
+
 proj.saveFormat3_2(file_name="Xcode-Python.xcodeproj/project.pbxproj")
 
